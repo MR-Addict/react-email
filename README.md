b6a32907530402d6767d34adbb683e0ff2716da1
MR-Addict <2750417853@qq.com>
2023-01-27 00:31:17 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# React Email Preview
+
+This is my first taste of [react-email](https://react.email/) writing an email with react.js and next.js. Which I think is cool.
+
+## 1. Preview
+
+|         Preview Email          |        Received Email        |
+| :----------------------------: | :--------------------------: |
+| ![Preview](images/preview.png) | ![Actual](images/actual.png) |
+
+## 2. What I think
+
+After the experience, I think the first react email offers more flexibility and options for us to write a beautiful email without much effort.
+
+But I think it's still got a lot of work to do. In my opinion, it was a little bit hard to write code than actual react, and there is still a lot of css style not working.
+
+After all, I still love it, react email would be on my first list if I got to write a good-looking email.
+
+## 3. How to Use
+
+Clone project:
+
+```bash
+git clone https://github.com/MR-Addict/react-email.git
+```
+
+Install packages:
+
+```bash
+npm install
+```
+
+Run dev
+
+```bash
+npm run dev
+```
---
+++ emails/profile.tsx
@@ -0,0 +1,77 @@
+import * as React from "react";
+import { Html } from "@react-email/html";
+import { Head } from "@react-email/head";
+import { Preview } from "@react-email/preview";
+import { Container } from "@react-email/container";
+import { Section } from "@react-email/section";
+import { Img } from "@react-email/img";
+import { Text } from "@react-email/text";
+import { Link } from "@react-email/link";
+import { Hr } from "@react-email/hr";
+
+export default function Email() {
+  return (
+    <Html>
+      <Head />
+      <Preview>MR.Addict's Profile</Preview>
+      <Section style={main}>
+        <Container style={container}>
+          <Section>
+            <Img src='https://avatars.githubusercontent.com/u/75357598' alt='avatar' style={avatar} />
+          </Section>
+          <Hr style={hr} />
+          <Section>
+            <Text style={paragraph}>
+              大家好，我是南京工业大学一名在校本科生。这是一个简单的自我介绍。我喜欢电子、网页设计和Linux。我自认为我个人有些完美主义，凡是我认可的事情总会花时间把事情做到最好。
+            </Text>
+          </Section>
+          <Hr style={hr} />
+          <Section>
+            <Text style={paragraph}>下面是我的个人主页，欢迎大家访问留言。</Text>
+            <Link href='https://mraddict.one' style={link}>
+              https://mraddict.one
+            </Link>
+          </Section>
+        </Container>
+      </Section>
+    </Html>
+  );
+}
+
+const main = {
+  backgroundColor: "#ffffff",
+  margin: "0 auto",
+};
+
+const container = {
+  width: "350px",
+  padding: "20px",
+  margin: "40px auto",
+  borderRadius: "10px",
+  border: "1px solid #eaeaea",
+};
+
+const avatar = {
+  width: "130px",
+  height: "130px",
+  margin: "20px auto",
+  borderRadius: "50%",
+};
+
+const paragraph = {
+  color: "#000",
+  fontSize: "14px",
+  lineHeight: "24px",
+  fontFamily: "BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu'",
+};
+
+const link = {
+  color: "#067df7",
+  textDecoration: "none",
+};
+
+const hr = {
+  border: "none",
+  borderTop: "1px solid #eaeaea",
+  width: "100%",
+};
